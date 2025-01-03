8d409227c95d45258334e81cd86477ac95ed9e02
Thomas Grainger <tagrain@gmail.com>
2025-01-03 09:21:27 +0000

    bump version to 0.2.2

--- taskgroup/__init__.py
+++ taskgroup/__init__.py
@@ -2,7 +2,7 @@
 backport of asyncio.TaskGroup, asyncio.Runner and asyncio.timeout
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = ["run", "Runner", "TaskGroup", "Timeout", "timeout", "timeout_at"]
 
